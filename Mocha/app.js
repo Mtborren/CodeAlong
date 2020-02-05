1e7c1cbff77722b5a8950ecc1897149714ad9300
vcjronijohn <ronijohnveronica@gmail.com>
2020-02-05 14:09:34 -0500

    test written, attempting to test now

---
+++ Mocha/app.js
@@ -0,0 +1,28 @@
+//app.js contains the functions for converting fahrenheit to celsius and vice versa.
+
+// let convert = {}; //this is used to expose/export our function to the test.js file.
+// cToF = function(celsius) {      //here the only parameter in the function named cToF is celsius, the value of celsius is then converted to fahrenheit.
+//     if(!Number.isInterger(celsius)) return undefined;
+//     return celsius * 9 / 5 + 32;
+// }
+
+// fToC = function(fahrenheit) {   //here the only parameter in the function fToC is fahrenheit, it converts this value to celsius.
+//     if(!Number.isInteger(fahrenheit)) return undefined;     //this states that if anything other than a number is entered it will return a value of undefined, otherwise it would proceed below to the equation and return the correct value.
+//     return (fahrenheit -32) * 5 / 9;
+// }
+
+
+let convert = {}; 
+//instead of using two different functions here, we're attaching the new object named convert to our existing functions as a method.
+convert.cToF = function(celsius) {      
+    if(!Number.isInterger(celsius)) return undefined;
+    return celsius * 9 / 5 + 32;
+}
+
+convert.fToC = function(fahrenheit) { 
+    if(!Number.isInteger(fahrenheit)) return undefined;     
+    return (fahrenheit -32) * 5 / 9;
+}
+
+module.exports = convert;
+//module.exports is how we tell javascript what object to return as the result of a 'require' call. 'require' will be placed in test.js and change the function names used in our tests to include the convert object.
---
+++ Mocha/test/test.js
@@ -0,0 +1,64 @@
+//file structure for test.js
+//A testing group named Temperature Conversion
+    //Two additional testing groups nested within, named cToF & fToC
+        //Each group will have three test cases, two numbers, and a non-integer test.
+
+// var assert = require('assert');
+// describe('Temperature Conversion', function() {     //part one - test group
+//     describe('cToF', function(){                    //part two - two testing groups within
+//     });
+//     describe('fToC', function(){
+//     })
+// })
+
+//assert.equal(actual, expected); - Java has built in assert assertion library, this one is used to test equality.
+
+//part 3 - test cases
+//fun fact* Fahrenheit and Celsius are equal at 40degree below zero
+
+// var assert = require('assert');
+// describe('Temperature Conversion', function() {     //part one
+//     describe('cToF', function(){                    //part two
+//         it('should convert -40 celsius to -40 fahrenheit', function() {
+//             assert.equal(-40, cTof(-40));
+//         });
+//         it('should convert 32 fahrenheit to 0 celsius', function()  {
+//             assert.equal(0, fToC(32));
+//         });
+//         it('should return undefined in no temperature is input', function() {
+//             assert.equal(undefined, fToC(''));
+//         });
+//     });
+//     describe('fToC', function(){
+//     });
+// });
+
+//now we have to add the 'require' call we named in app.js to enable the files to communicate.
+
+let convert = require('../app.js')
+let assert = require('assert'); //change variable from var to let.
+
+describe('Temperature Conversion', function() {     //part one
+    describe('cToF', function(){                    //part two
+        it('should convert -40 celsius to -40 fahrenheit', function() {
+            assert.equal(-40, convert.cTof(-40));   //add convert to equation to be able to pull info from app.js
+        });
+        it('should convert 32 fahrenheit to 0 celsius', function()  {
+            assert.equal(0, convert.fToC(32));
+        });
+        it('should return undefined if no temperature is input', function() {
+            assert.equal(undefined, convert.fToC(''));
+        });
+    });
+    describe('fToC', function(){
+        it('should convert -40 fahrenheit to -40 celsius', function() {
+            assert.equal(-40, convert.fToC(-40));
+        });
+        it('should convert 0 celsius to 32 fahrenheit', function() {
+            assert.equal(0, convert.fToC(32));
+        });
+        it('should return undefined if no temperature is input', function() {
+            assert.equal(undefined, convert.fToC('a'));
+        });
+    });
+});
